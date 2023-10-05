ab347e588fd9bd01e7c0cefa22d171eb99d34edd
Yoonh <yoonhogeon06@gmail.com>
2023-10-05 19:23:02 +0900

    fixed: main.py

--- main.py
+++ main.py
@@ -17,18 +17,31 @@ def mul(a, b):
 
 def div(a, b):
     if a == 0:
-        return ("a가 0이니 계산 X")
+        print ("a가 0이니 계산 X")
     elif b == 0:
-        return ("b가 0이니 계산 X")
+        print ("b가 0이니 계산 X")
     else:
-        return (a/b)
+        print (a/b)
 
 
 
 
 def main():
+    operator = int(input("1. 덧셈 2. 뺄셈 3. 곱셈 4. 나눗셈"))
+    a = int(input("a를 입력하세요."))
+    b = int(input("b를 입력하세요."))
 
+    if operator == 1:
+        add(a, b)
 
+    elif operator == 2:
+        sub(a, b)
+
+    elif operator == 3:
+        mul(a, b)
+
+    elif operator == 4:
+        div(a,b)
 
 if __name__ == "__main__":
     main()
